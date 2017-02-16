96775c7ad0806ad8390a56eee64befe8d6bfa34f
Benjamin Allen <benjaminallen1985@yahoo.com>
2017-02-16 02:51:09 -0600

    Added Array ViewState project

---
+++ ViewStateArrayPopulation/CS-ASP_021/CS-ASP_021/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CS_ASP_021.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Type a value into boxes and click &quot;add&quot; to save them, then click &quot;retrive&quot; to show them. IndexOutOfRange not handled.<br />
+        <br />
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+&nbsp;
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+&nbsp;
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+&nbsp;
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="addButton" runat="server" Text="Add" OnClick="addButton_Click" />
+&nbsp;
+        <asp:Button ID="retrieveButton" runat="server" Text="Retrieve" OnClick="retrieveButton_Click" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ViewStateArrayPopulation/CS-ASP_021/CS-ASP_021/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CS_ASP_021
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            string[] values = new string[5] { TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text };
+            ViewState.Add("MyValues", values);
+            resultLabel.Text = "Values added....";
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+        }
+
+        protected void retrieveButton_Click(object sender, EventArgs e)
+        {
+            string[] values = (string[])ViewState["MyValues"];
+            TextBox1.Text = values[0];
+            TextBox2.Text = values[1];
+            TextBox3.Text = values[2];
+            TextBox4.Text = values[3];
+            TextBox5.Text = values[4];
+
+            resultLabel.Text = "Values retrieved...";
+        }
+    }
+}
